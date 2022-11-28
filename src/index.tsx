c8b96faa6be462ac33fd3ab0370bdf65a56a439a
Le Ba Toan <kenzo.devweb@gmail.com>
2022-11-28 11:48:05 +0700

    bug

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,6 @@
-import { StrictMode } from "react";
-import * as ReactDOMClient from "react-dom/client";
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = ReactDOMClient.createRoot(rootElement);
-
-root.render(
-  <StrictMode>
- 
-      <App />
-  
-  </StrictMode>
-);
+const container = document.getElementById('app');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(<App />);
